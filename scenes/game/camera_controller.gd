a7c700f99390f0481008631027979e045ec9b5d8
data5 <99661153+FixedData@users.noreply.github.com>
2026-05-09 02:22:49 -0600

    camera controller cleanup
    Camera controller no longer crashes with no parent
    Improved docs
    Cleaned up script
    Cleaned up export vars
    
    When in 2d mode makes use of the camera2D's built in position smoothing funcs instead of replicating
    
    and now has a icon wow

--- scenes/game/camera_controller.gd
+++ scenes/game/camera_controller.gd
@@ -1,42 +1,58 @@
+@icon("uid://bpfewkvnsj6h5")
 extends Node
 class_name CameraController
 
-@onready var noise = FastNoiseLite.new()
-@onready var rand = RandomNumberGenerator.new()
 
-@export_subgroup("Essentials")
-
+## A camera2D node that this will control.
 @export var parent_2D: Camera2D
+
+## A camera3D node that this will control.
 @export var parent_3D: Camera3D
 
+#can we change these var names some time
+@export_category('Zoom Settings')
+## The intended camera zoom. The camera will automatically attempt to lerp to this zoom.
 @export_custom(PROPERTY_HINT_LINK, 'x') var target_zoom: Vector2 = Vector2(1, 1)
-@export_range(1, 25) var lerp_weight: float = 5
+## The rate the camera's current [code]zoom[/code] will lerp to [code]target_zoom[/code]
+@export_range(1, 64) var lerp_weight: float = 5
 
+## If true the camera will attempt to lerp to [code]target_zoom[/code].
 @export var lerping = true
+## If true, the camera's view smoothly moves towards its target position at [code]position_smoothing_speed[/code].
+@export var position_smoothing:bool = true : 
+	set(v):
+		if parent_2D:
+			parent_2D.position_smoothing_enabled = v
+		
+		position_smoothing = v
+## Speed in pixels per second of the camera's smoothing effect when [code]position_smoothing_enabled[/code] is true.
+@export var position_smoothing_speed:float = 3.0 : 
+	set(v):
+		if parent_2D:
+			parent_2D.position_smoothing_speed = v
+		position_smoothing_speed = v
 
-@export var position_smoothing:bool = true
-@export var position_smoothing_speed:float = 3.0
+@export_group("Shake Settings")
 
-@export_subgroup("Shaking")
-
-@export var shake_time: float = 0.0
-@export var shaking: bool = false
-
-var default_offset: Vector2
-var _position_2d: Vector2 = Vector2.ZERO
-var _position_3d: Vector3 = Vector3.ZERO
-
-# How quickly to move through the noise
+## How quickly to move through the noise
 @export var shake_speed: float = 30.0
-# Noise returns values in the range (-1, 1)
-# So this is how much to multiply the returned value by
+## Noise returns values in the range (-1, 1)
+## So this is how much to multiply the returned value by
 @export var shake_strength: float = 60.0
-# The starting range of possible offsets using random values
+## The starting range of possible offsets using random values
 @export var random_shake_strength: float = 30.0
-# Multiplier for lerping the shake strength to zero
+## Multiplier for lerping the shake strength to zero
 @export var shake_decay_rate: float = 3.0
 
+var default_offset: Vector2
+var shake_time: float = 0.0
+var shaking: bool = false
+
+var noise = FastNoiseLite.new()
+var rand = RandomNumberGenerator.new()
 var noise_i: float = 0.0
+
+var _position_3d: Vector3 = Vector3.ZERO # needed for parity with camera 2d
 
 var position:Variant:
 	set(value):
@@ -50,21 +66,19 @@ var zoom:Variant :
 	get:
 		return get_zoom()
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	 
 	if not parent_2D and not parent_3D:
-		assert(false,"Camera controller is missing a camera parent")
+		printerr('(Camera Controller): No parent was assigned.')
 	
 	if parent_2D:
-		parent_2D.position_smoothing_enabled = false
 		default_offset = parent_2D.offset
-		_position_2d = parent_2D.position
+		parent_2D.position_smoothing_enabled = position_smoothing
+		parent_2D.position_smoothing_speed = position_smoothing_speed
 	elif parent_3D:
 		default_offset = Vector2(parent_3D.h_offset,parent_3D.v_offset)
 		_position_3d = parent_3D.position
 
-func get_direct():
+func get_direct() -> Variant:
 	if parent_2D: 
 		return parent_2D
 	elif parent_3D:
@@ -84,46 +98,39 @@ func set_zoom(value:Variant):
 			parent_3D.fov = value.x
 		if value is float:
 			parent_3D.fov = value
-			
-			
 
 func get_zoom() -> Variant:
 	if parent_2D: return parent_2D.zoom
-	
-	return parent_3D.fov
+	elif parent_3D: return parent_3D.fov
+	return Vector2.ZERO
 
 func set_position(value:Variant):
 	if value == null: return
 	
 	if parent_2D:
 		if value is Vector3:
-			value = vec3_to_vec2(value)
-		
-		if position_smoothing:
-			_position_2d = value
-		else:
-			parent_2D.position = value
-		
+			value = Vector2(value.x, value.y)
+		parent_2D.position = value
+	
 	elif parent_3D:
 		if value is Vector2:
 			if position_smoothing:
 				_position_3d.x = value.x
 				_position_3d.y = value.y
 				return
-				
+			
 			parent_3D.position.x = value.x
 			parent_3D.position.y = value.y
 		if value is Vector3:
 			if position_smoothing:
 				_position_3d = value
 				return
-				
 			parent_3D.position = value
 
 func get_position() -> Variant:
 	if parent_2D: return parent_2D.position
-	
-	return parent_3D.position
+	elif parent_3D: return parent_3D.position
+	return Vector2.ZERO
 
 func _process(delta):
 	if parent_2D: update_2D(delta)
@@ -132,9 +139,6 @@ func _process(delta):
 func update_2D(delta):
 	if lerping:
 		parent_2D.zoom = Global.frame_independent_lerp(parent_2D.zoom, target_zoom, lerp_weight, delta)
-	
-	if position_smoothing:
-		parent_2D.position = lerp_position(parent_2D.position, _position_2d, delta)
 	
 	if shaking:
 		
@@ -145,7 +149,6 @@ func update_2D(delta):
 		
 		shake_time -= delta
 		if shake_time <= 0: end_shake()
-		
 
 func update_3D(delta):
 	if lerping:
@@ -164,7 +167,6 @@ func update_3D(delta):
 		
 		shake_time -= delta
 		if shake_time <= 0: end_shake()
-
 
 func shake(amount: int, time: float):
 	shake_time = time
@@ -194,9 +196,6 @@ func get_noise_offset(delta: float, speed: float, strength: float) -> Vector2:
 		noise.get_noise_2d(1, noise_i) * strength,
 		noise.get_noise_2d(100, noise_i) * strength
 	)
-
-func vec3_to_vec2(vec3: Vector3) -> Vector2:
-	return Vector2(vec3.x, vec3.y)
 
 func lerp_position(cur:Variant, intended:Variant, delta:float):
 	var c = position_smoothing_speed * delta
