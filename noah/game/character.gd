52fc32f5648e2914914008c4e666a0f9af8a9f0f
noxhy <58011428+noxhy@users.noreply.github.com>
2026-05-13 15:26:22 -0400

    more documentation

--- noah/game/character.gd
+++ noah/game/character.gd
@@ -1,4 +1,5 @@
 @icon("uid://ca6uggp4ff1q2")
+@tool
 extends Node
 ## The main class for characters in a song, such as the player, enemy, or metronome.
 class_name Character
@@ -18,6 +19,8 @@ enum AnimContext {
 }
 
 @export_group("Animation Data")
+## Dictionary of given animation id's and their [SpriteFrames] animation.
+## [br][br][b]Example:[/b] [code]{"idle": "BF idle dance"}[/code]
 @export var animation_names: Dictionary[StringName, StringName] = {}
 @export var offsets: Dictionary[StringName, Vector2] = {}
 @export var hold_frames: Dictionary[StringName, int] = {}
@@ -27,6 +30,8 @@ enum AnimContext {
 @export var dance_animations: Array[StringName] = [&"idle"]
 ## How often [b](in steps)[/b] the dance will be played.
 @export_range(1, 1, 1, "suffix:steps", "or_greater") var dance_rate: int = 8
+## When calling an animation, the id will be appended to this value.
+## [br][br][b]Example:[/b] [code]"left"[/code] → [code]"mom_left"[/code]
 @export var animation_prefix: StringName = &""
 ## How many steps an animation can play before being able to revert to idle.
 @export_custom(PROPERTY_HINT_NONE, 'suffix:steps') var sing_duration: float = 6
@@ -43,7 +48,7 @@ enum AnimContext {
 var current_dance: int = 0
 ## The current animation ID.
 var current_animation: StringName = dance_animations[0]
-## The current [AnimContext]
+## The current [enum AnimContext]
 var current_context: AnimContext = AnimContext.NONE
 var can_dance: bool = true
 ## Used to make an animation loop at the given [code]hold_frame[/code] until given another animation.
@@ -63,6 +68,7 @@ func _ready():
 	
 	if animation_player is AnimateSymbol:
 		animation_player.connect(&"finished", self._on_animation_finished)
+		animation_player.connect(&"animation_changed", self._on_animation_changed)
 	else:
 		animation_player.play()
 		animation_player.connect(&"animation_finished", self._on_animation_finished)
@@ -71,7 +77,9 @@ func _ready():
 func _on_animation_finished():
 	holding = true
 
-
+## Plays an animation with the given context. Setting [param restart] to [code]true[/code] will replay the animation from the beginning.
+## Setting a [param time] will make the animation play within the given time.
+## [br][br]See [enum AnimContext] for information of animation contexts.
 func play_animation(anim_id: StringName = &"", context: AnimContext = AnimContext.NONE, restart: bool = true, time: float = -1.0):
 	if process_mode == Node.PROCESS_MODE_DISABLED or current_context == AnimContext.LOCKED and !animation_player:
 		return
@@ -129,14 +137,21 @@ func play_animation(anim_id: StringName = &"", context: AnimContext = AnimContex
 
 
 func _process(delta: float) -> void:
-	if holding:
-		hold_animation()
+	if Engine.is_editor_hint():
+		if animation_player:
+			if animation_player is AnimatedSprite2D:
+				pass
 	
-	sing_time -= delta
-	if sing_time <= 0 and !can_dance:
-		can_dance = true
+	if !Engine.is_editor_hint():
+		if holding:
+			hold_animation()
+		
+		sing_time -= delta
+		if sing_time <= 0 and !can_dance:
+			can_dance = true
 
 
+## Gets the [SpriteFrames] animation name of the given [param anim_id] in [member animation_names].
 func get_animation_name(anim_id: StringName = &""):
 	return animation_names.get(anim_id, &"")
 
@@ -193,7 +208,7 @@ func dance(restart: bool = true, time: float = -1) -> void:
 	
 	current_dance = wrapi(current_dance + 1, 0, dance_animations.size())
 
-
+## Sets the sing timer, characters cannot return to idle until this timer is finished.
 func set_sing_timer(time: float = -1):
 	if time == -1:
 		time = sing_duration * GameManager.seconds_per_step
@@ -201,6 +216,7 @@ func set_sing_timer(time: float = -1):
 	sing_time = time
 	can_dance = false
 
-func on_step_hit(current_step: int, measure_relative: int):  ##basic song hooks this to the conductor
+## Helper function for [code]basic_song.gd[/code] to call idle whenever possible
+func on_step_hit(current_step: int, measure_relative: int):  
 	if dance_rate > 0 and measure_relative % dance_rate == 0:
 		dance()
