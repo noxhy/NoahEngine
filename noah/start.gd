501ca0891f3e50f0c0179699d2c5fa38075b22d2
noxhy <58011428+noxhy@users.noreply.github.com>
2026-06-20 03:36:21 -0400

    i need to make a mod menu

--- noah/start.gd
+++ noah/start.gd
@@ -2,4 +2,12 @@ extends Node2D
 
 # Meant to be replaced
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed(&"chart_editor"):
+		get_tree().change_scene_to_file(Constants.CHART_EDITOR_SCENE)
+
+
+func _on_button_pressed() -> void:
+	$FileDialog.popup()
