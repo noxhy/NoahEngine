3fe450f706d7028c33b160fee09b43cef98f640b
noxhy <58011428+noxhy@users.noreply.github.com>
2026-06-30 03:20:40 -0400

    Update transition.gd

--- noah/transitions/transition.gd
+++ noah/transitions/transition.gd
@@ -3,7 +3,7 @@ class_name Transition extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 signal paused
-signal finished
+signal finished(trans_name: StringName)
 
 func _ready() -> void:
 	animation_player.connect("animation_finished", self.finish)
@@ -24,5 +24,5 @@ func pause() -> void:
 	paused.emit()
 
 
-func finish() -> void:
+func finish(trans_name: StringName) -> void:
 	finished.emit()
