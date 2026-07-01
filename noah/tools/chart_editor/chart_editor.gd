21af10ab714a1f2cf6666cd0d555597ae62be0e8
data5 <99661153+FixedData@users.noreply.github.com>
2026-07-01 01:18:34 -0600

    Update chart_editor.gd

--- noah/tools/chart_editor/chart_editor.gd
+++ noah/tools/chart_editor/chart_editor.gd
@@ -1468,12 +1468,15 @@ func update_waveforms(time: float = 0):
 			var L: float = max(time, 0)
 			var R: float = min(time + conductor.numerator * conductor.get_seconds_per_beat(), instrumental.stream.get_length())
 			waveform.time = (L * 1000) / 7.8
-			waveform.duration = (conductor.get_seconds_per_beat() * 1000) / 1.95
+			waveform.duration = (R - L) * 130
+			#waveform.duration = (conductor.get_seconds_per_beat() * 1000) / 1.95
 			
+			waveform.height = %Grid.grid_size.x * 4
 			waveform.width = time_to_y_position(R) - time_to_y_position(L)
 			waveform.position = %Grid.get_real_position(Vector2(ChartManager.strum_data[id]["strums"][1] - 1.5, 0))
 			waveform.position.y += time_to_y_position(L)
 			waveform.dirty = true
+			
 
 
 func _on_chart_snap_value_changed(value: float) -> void:
