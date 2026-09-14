ff007dd7459d0785d9713ab842314c67bcc7cc6b
data5 <99661153+FixedData@users.noreply.github.com>
2026-09-13 22:50:30 -0600

    noteskin docs

--- noah/resources/ui_skin.gd
+++ noah/resources/ui_skin.gd
@@ -3,13 +3,29 @@ extends Resource
 class_name UISkin
 
 @export_subgroup("Textures")
+
+## The textures of the rating popups.
+## [br][br]The animations should be [code]miss, shit, bad, good, sick[/code].
+## [br][br]They can also have a FC alternative named as [code]fc_RANK[/code]
 @export var rating_texture: SpriteFrames
+
+## The textures of the rating number popups.
+## [br][br]The animations should be [code]0[/code] to [code]9[/code].
+## [br][br]They can also have a FC alternative named as [code]fc_NUMBER[/code]
 @export var numbers_texture: SpriteFrames
+
+## Whether the notes filtering should be set to [code]NEAREST_NEIGHBOR[/code] to keep it crisp when scaled.
 @export var pixel_texture: bool = false
 
 @export_subgroup("Texture Scaling")
+
+## Multiplicative scaling applied to the notes and strums.
 @export var rating_scale: float = 1.0
+
+## Multiplicative scaling applied to the notes and strums.
 @export var numbers_scale: float = 1.0
+
+## The distance between each number. Gets [member rating_scale] Automatically applied
 @export var numbers_spacing: float = 64
 
 @export_subgroup("Offsets")
@@ -17,5 +33,8 @@ class_name UISkin
 @export var offsets: Dictionary[StringName, Vector2] = {}
 
 @export_subgroup("Scenes")
+## [color=khaki]OPTIONAL:[/color]
+## The path to a countdown animation player scene.
 @export_file var countdown: String
+## The path to a scene to be loaded when paused.
 @export_file var pause_scene: String = "uid://djhqiluiy02ao"
