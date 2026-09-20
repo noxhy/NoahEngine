08b46798f5bd9a1f3ef986ca9be5011484800e9a
noxhy <58011428+noxhy@users.noreply.github.com>
2026-09-20 15:17:38 -0400

    Update constants.gd

--- noah/constants.gd
+++ noah/constants.gd
@@ -4,8 +4,9 @@ extends Node
 var CHART_EDITOR_SCENE: String = "uid://c3lux2ajoe1g6"
 var EVENT_EDITOR_SCENE: String = "uid://cq6xqods6w7lw"
 var START_MENU_SCENE: String = "uid://cmn3noo2keqes"
-#region Event Data
+var OPTIONS_MENU_SCENE: String = "uid://clg58h4buylla"
 
+#region Event Data
 var EVENT_DATA: Dictionary = {
 	"camera_position": {
 		"parameters": ["Marker Index", "(Optional = 0) Duration", "(Optional = CLASSIC) Easing Type"],
